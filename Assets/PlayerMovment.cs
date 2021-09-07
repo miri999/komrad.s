5b01b8985520716741049d8b8df7127173359841
miri999 <63510319+miri999@users.noreply.github.com>
2021-09-07 18:09:52 -0400

    cleaned up a bit

---
+++ Assets/PlayerMovment.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    public Rigidbody controller;
+
+    public float speed = 12f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public float jumpHeight = 3f;
+    public float gravity = 10;
+
+
+    public Vector3 velocity;
+    bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller.freezeRotation = true;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        else if (!isGrounded)
+        {
+            velocity.y -= gravity * Time.deltaTime;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        //Quaternion zw = this.transform.rotation;
+        controller.velocity = (move * speed * Time.deltaTime)+(this.transform.rotation * velocity * Time.deltaTime);
+
+
+
+
+    }
+    private void Update()
+    {
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Abs(jumpHeight * -2 * gravity);
+        }
+        
+    }
+
+}
---
+++ Assets/mouse_Look.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouse_Look : MonoBehaviour
+{
+
+
+    public float mouseSensitivity = 100f;
+    float xRotation = 0f;
+    public Transform playerBody;
+    private GameObject prev;
+    Material orig;
+    private Material highlight;
+    public Material UngUhn;
+    public Material yes;
+    public LayerMask mask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        RaycastHit hit;
+        Ray ray = this.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+
+        if (prev == null)
+        {}
+        else
+        {
+            prev.GetComponent<Renderer>().material = orig;
+        }
+        
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, mask) && Input.GetMouseButtonUp(0))
+        {
+            Transform objectHit = hit.transform;
+            if(Vector3.Distance(this.transform.position, objectHit.position) < 50)
+            {
+                GameObject hitgo = objectHit.gameObject;
+                playerBody.rotation = objectHit.transform.rotation;
+            }
+        }else if (Physics.Raycast(ray, out hit, Mathf.Infinity, mask) && Input.GetMouseButton(0))
+        {
+            
+            Transform objectHit = hit.transform;
+            if(Vector3.Distance(this.transform.position, objectHit.position) > 50)
+            {
+                highlight = UngUhn;
+            }
+            else
+            {
+                highlight = yes;
+            }
+            GameObject hitgo = objectHit.gameObject;
+            orig = hitgo.GetComponent<Renderer>().material;
+            hitgo.GetComponent<Renderer>().material = highlight;
+            prev = hitgo;
+        }
+    }
+}
